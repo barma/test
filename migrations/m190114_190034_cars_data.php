a9421c61af7f62edfbf19aed3dc22fd9fc4e466c
vagrant <vagrant@yii.test>
2019-01-15 21:27:08 +0000

    bugfix

--- migrations/m190114_190034_cars_data.php
+++ migrations/m190114_190034_cars_data.php
@@ -12,12 +12,12 @@ class m190114_190034_cars_data extends Migration
      */
     public function safeUp()
     {
-        $this->insert('car', ['title' => 'c180', 'url' => 'c180', 'category' => 1, 'price' => '50']);
-        $this->insert('car', ['title' => 's600', 'url' => 's600', 'category' => 1, 'price' => '150']);
-        $this->insert('car', ['title' => 'almera', 'url' => 'almera', 'category' => 2, 'price' => '250']);
-        $this->insert('car', ['title' => 'note', 'url' => 'note', 'category' => 2, 'price' => '350']);
-        $this->insert('car', ['title' => 'c3', 'url' => 'c3', 'category' => 3, 'price' => '450']);
-        $this->insert('car', ['title' => 'c4', 'url' => 'c4', 'category' => 3, 'price' => '550']);
+        $this->insert('car', ['title' => 'c180', 'url' => 'c180', 'categoryId' => 1, 'price' => '50']);
+        $this->insert('car', ['title' => 's600', 'url' => 's600', 'categoryId' => 1, 'price' => '150']);
+        $this->insert('car', ['title' => 'almera', 'url' => 'almera', 'categoryId' => 2, 'price' => '250']);
+        $this->insert('car', ['title' => 'note', 'url' => 'note', 'categoryId' => 2, 'price' => '350']);
+        $this->insert('car', ['title' => 'c3', 'url' => 'c3', 'categoryId' => 3, 'price' => '450']);
+        $this->insert('car', ['title' => 'c4', 'url' => 'c4', 'categoryId' => 3, 'price' => '550']);
     }
 
     /**
